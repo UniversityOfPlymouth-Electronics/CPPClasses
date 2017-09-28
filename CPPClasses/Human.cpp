a0108787e1a345856723e53712570a24a32e899f
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2017-09-28 16:45:22 +0100

    Intentionally blank

---
+++ CPPClasses/Human.cpp
@@ -0,0 +1,9 @@
+//
+//  Human.cpp
+//  CPPClasses
+//
+//  Created by Nicholas Outram on 28/09/2017.
+//  Copyright © 2017 Nicholas Outram. All rights reserved.
+//
+
+#include "Human.hpp"
---
+++ CPPClasses/Human.hpp
@@ -0,0 +1,14 @@
+//
+//  Human.hpp
+//  CPPClasses
+//
+//  Created by Nicholas Outram on 28/09/2017.
+//  Copyright © 2017 Nicholas Outram. All rights reserved.
+//
+
+#ifndef Human_hpp
+#define Human_hpp
+
+#include <stdio.h>
+
+#endif /* Human_hpp */
